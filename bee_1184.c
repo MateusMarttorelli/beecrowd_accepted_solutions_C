041412461cf2bc2055815f9000efe7f914a6899e
MateusMarttorelli <88115645+MateusMarttorelli@users.noreply.github.com>
2022-05-16 11:18:09 -0300

    Updating

---
+++ bee_1184.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+
+	float M[12][12], soma = 0, media;
+	int i, j;
+	char O;
+
+	scanf("%c", &O);
+
+	for (i = 0; i < 12; i++) {
+		for (j = 0; j < 12; j++) {
+
+			scanf("%f", &M[i][j]);
+		}
+	}
+
+	if (O == 'S') {
+
+		for (i = 0; i < 12; i++) {
+			for (j = 0; j < i; j++) {
+
+				soma += M[i][j];
+			}
+		}
+		printf("%.1f\n", soma);
+	}
+	else if (O == 'M') {
+
+		for (i = 0; i < 12; i++) {
+			for (j = 0; j < i; j++) {
+
+				soma += M[i][j];
+			}
+		}
+		media = soma / 66;
+		printf("%.1f\n", media);
+	}
+
+	return 0;
+}
